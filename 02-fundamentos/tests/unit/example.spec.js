4e75633dda9813e0b527adc478fe1f28ca3c7200
Álvaro Navarro <31630723+thesnakebite@users.noreply.github.com>
2023-05-01 19:03:58 +0200

    Expect(Jest/Api/Expect)

--- 02-fundamentos/tests/unit/example.spec.js
+++ 02-fundamentos/tests/unit/example.spec.js
@@ -1,16 +1,12 @@
 describe('Example Component', () => {
   test('Debe ser mayor a 10', () => {
     // Arreglar
-    const value = 5;
+    let value = 8;
 
     // Estimulo
     value = value + 2
 
     // Observar el resultado
-    if(value > 10){
-      // TODO: todo bien!
-    } else {
-      throw `${value} no es mayor a 10`
-    }
+    expect(value).toBeGreaterThan(10)
   })
 })
