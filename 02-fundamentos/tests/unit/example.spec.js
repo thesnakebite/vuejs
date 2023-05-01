16193d31c5de95c6af0dd3c5a7ae50a8a0ef6632
Álvaro Navarro <31630723+thesnakebite@users.noreply.github.com>
2023-05-01 18:49:15 +0200

    My first test

--- 02-fundamentos/tests/unit/example.spec.js
+++ 02-fundamentos/tests/unit/example.spec.js
@@ -1,12 +1,16 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+describe('Example Component', () => {
+  test('Debe ser mayor a 10', () => {
+    // Arreglar
+    const value = 5;
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+    // Estimulo
+    value = value + 2
+
+    // Observar el resultado
+    if(value > 10){
+      // TODO: todo bien!
+    } else {
+      throw `${value} no es mayor a 10`
+    }
   })
 })
