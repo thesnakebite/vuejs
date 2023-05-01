db2b16eeea220df9c68a20f7eec09d5be09f32dc
Álvaro Navarro <31630723+thesnakebite@users.noreply.github.com>
2023-05-01 11:20:58 +0200

    computed properties

--- 02-fundamentos/src/components/Counter.vue
+++ 02-fundamentos/src/components/Counter.vue
@@ -2,6 +2,13 @@
     <div>
          <h2>Counter</h2>
         <p>{{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
+        <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+        <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+        <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+        <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+        <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
+
+
     </div>
    
 </template>
@@ -13,6 +20,22 @@
         data() {
             return {
                 counter: 5
+            }
+        },
+
+        methods: {
+            getSquareValue(){
+                console.log('getSquareValue')
+                return this.counter * this.counter
+            }
+        },
+        // Las propiedades computadas se guardan en el caché
+        // lo cuál ayuda en el proceso, obtenemos el mismo resultado
+        // pero solo se llamo una sola vez, esa es la principal diferencia.
+        computed: {
+            squareCounter(){
+                console.log('computed squareCounter')
+                return this.counter * this.counter
             }
         }
     } 
