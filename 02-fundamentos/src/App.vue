5c2f5b1ff61071958f02cafea90aa36d49f46a18
Álvaro Navarro <31630723+thesnakebite@users.noreply.github.com>
2023-05-01 09:52:30 +0200

    My first component

--- 02-fundamentos/src/App.vue
+++ 02-fundamentos/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Counter />
+  </div>
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from '@/components/Counter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Counter
   }
 }
 </script>
---
+++ 02-fundamentos/src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+         <h2>Counter</h2>
+        <p>10</p>
+    </div>
+   
+</template>
+
+<script>
+    export default {
+        name:'Counter'
+    }
+
+</script>
+
+<style scoped>
+
+</style>
