c4f5d7dd736e39add029cf7f75cf4e7bd0c05725
alvaro.navarro <alvaro.navarro@manifiesto.biz>
2023-05-02 10:07:46 +0200

    Check value of an html tag

--- 02-fundamentos/tests/unit/components/counter.spec.js
+++ 02-fundamentos/tests/unit/components/counter.spec.js
@@ -1,11 +1,22 @@
 import { shallowMount } from '@vue/test-utils'
 import Counter from '@/components/Counter.vue'
+import { h } from 'vue'
 
 describe('Counter Component', () => {
-    test('Debe ser identico al image', () => {
-        const wrapper = shallowMount( Counter )
+    // test('Debe ser identico al image', () => {
+    //     const wrapper = shallowMount( Counter )
 
-        expect ( wrapper.html() ).toMatchSnapshot()
+    //     expect ( wrapper.html() ).toMatchSnapshot()
+
+    // })
+    test('h2 debe de tener el valor por defecto "Counter"', () => {
+        const wrapper = shallowMount ( Counter )
+
+        expect( wrapper.find('h2').exists()).toBeTruthy()
+
+        const h2Value = wrapper.find('h2').text()
+        
+        expect(h2Value).toBe('Counter')
 
     })
 })
