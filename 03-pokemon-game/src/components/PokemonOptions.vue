6b024da85f0503296f2c26dd73a9bfb70639c530
Álvaro Navarro <31630723+thesnakebite@users.noreply.github.com>
2023-05-07 18:24:09 +0200

    Pokemon and its components and styles

---
+++ 03-pokemon-game/src/components/PokemonOptions.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="options-container">
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    /* Pokemon Options */
+    ul {
+        list-style-type: none;
+        margin: 20px 0 0 0;
+        padding: 0;
+    }
+    
+    li {
+        background-color: white;
+        border-radius: 5px;
+        border: 1px solid rgba(0, 0, 0, 0.2);
+        cursor: pointer;
+        margin-bottom: 10px;
+        width: 250px;
+    }
+
+    li:hover {
+        background-color: rgba(0, 0, 0, 0.05);
+    }
+
+    .options-container {
+        display: flex;
+        justify-content: center;
+    }
+
+</style>
---
+++ 03-pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>¿Quién es este pokémon?</h1>
+            <PokemonPicture/>
+            <PokemonOptions/>
+    </div>
+    
+</template>
+
+<script>
+    import PokemonPicture from '@/components/PokemonPicture'
+    import PokemonOptions from '@/components/PokemonOptions'
+
+    export default {
+        components: {
+            PokemonPicture,
+            PokemonOptions,
+        }
+
+    }
+</script>
